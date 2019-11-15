12939cf8c6508644a593efe6a7060e5a1bcdd98e
BilelBouquoyoue <56070747+BilelBouquoyoue@users.noreply.github.com>
2019-11-15 16:45:45 +0100

    Add files via upload

---
+++ JUnitModeleSnake.java
@@ -0,0 +1,34 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ModeleSnakeTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSnake() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreerOeuf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOnKeyPressed() {
+		fail("Not yet implemented");
+	}
+
+}
